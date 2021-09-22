200e36211603433d9e2c4722c062f6c549319fa6
Alex Rocha <81331643+alexrocha97@users.noreply.github.com>
2021-09-21 21:21:10 -0300

    Exclusão de um atributo da tarefa

---
+++ server/To_do/Migrations/20210922002012_exlu.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace To_do.Migrations
+{
+    public partial class exlu : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prioridades",
+                table: "Tarefas");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Prioridades",
+                table: "Tarefas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- server/To_do/Models/Tarefas.cs
+++ server/To_do/Models/Tarefas.cs
@@ -9,7 +9,7 @@ namespace To_do.Models
         public int Id { get; set; }
         public string Tarefa { get; set; }
         public string Prioridade { get; set; }
-        public Prioridade Prioridades { get; set; }
+        //public Prioridade Prioridades { get; set; }
         //public bool? Verificar { get; set; }
         public DateTime Data_tarefa { get; set; }
         public List<Users> Users { get; set; }
