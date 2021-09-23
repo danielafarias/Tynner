b218c0d8b07e0da72d3e158f979c7764489abe09
Daniela Farias <farias.danicris@gmail.com>
2021-09-22 23:29:18 -0300

    Register pronto, faltando apenas a chamada da API e Footer feito, precisando apenas arrumar um detalhe

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,19 +1,24 @@
-import { AppBar, Toolbar, Grid, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Grid } from '@material-ui/core';
 import styles from '../styles/pages/Header.module.scss';
 
-export default function Header() {
+export default function Header(props: any) {
   return (
-    <div className={styles.grow}>
+    <header className={styles.grow}>
       <AppBar className={styles.bar} position="fixed">
         <Toolbar>
-          <Grid container justifyContent="flex-end" alignItems="center">
-         
+          <Grid container justifyContent="space-between" alignItems="center">
             <Grid item>
-              aaaaa
+              <img src="images/tynner.svg" className={styles.logoIcon} alt="Logo do site"/>
+            </Grid>
+            <Grid item>
+              {props.searchBar}
+            </Grid>
+            <Grid item>
+              {props.buttons}
             </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
-    </div>
+    </header>
   );
 }
--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,9 +1,77 @@
-import Header from '../components/Header';
+import Header from "../components/Header";
+import { TextField, Typography, Button, Grid } from "@material-ui/core";
+import React from "react";
+import styles from "../styles/pages/Register.module.scss";
+import Footer from "../components/Footer";
 
 export default function Register() {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+  const [username, setUsername] = React.useState("")
+  const [email, setEmail] = React.useState("")
+  const [password, setPassword] = React.useState("")
+  return (
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <form>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item>
+              <Typography variant="h5">Registrar-se</Typography>
+            </Grid>
+            <Grid item>
+              <TextField
+                required
+                label="Nome"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                variant="filled"
+                type="text"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                required
+                label="E-mail"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                variant="filled"
+                type="text"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                required
+                label="Senha"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                variant="filled"
+                type="text"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <Button type="submit">Confirmar</Button>
+            </Grid>
+          </Grid>
+        </form>
+      </main>
+        <Footer />
+    </div>
+  );
 }
