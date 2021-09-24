d27b5e7917b78756cbb05d7ad27468cee7466553
Marcos Domingues Junior <marcos_domingues@hotmail.com>
2021-09-23 23:33:09 -0300

    continuação da index

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,32 +1,103 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/pages/index.module.scss";
-import { Container, Grid, Typography, Button } from "@material-ui/core";
+import styles from "../styles/pages/Index.module.scss";
+import {
+  Container,
+  Grid,
+  Typography,
+  Button,
+  TextField,
+} from "@material-ui/core";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Tynner</title>
         <meta name="Tynner" content="Tynner - Seu planner vintage" />
       </Head>
 
       <main>
-        <div className={styles.login}>
-          <Grid container justifyContent="space-between" alignItems="center" direction="column">
-            <Grid item className={styles.login__header}>
-              <img
-                src="images/tynner.svg"
-               
-              />
+        <div className={styles.head}>
+          <Grid
+            container
+            justifyContent="flex-start"
+            alignItems="center"
+            direction="column"
+          >
+            <Grid item className={styles.head__img}>
+              <img src="images/tynner.svg" />
               <Typography>
-                <h1 className={styles.login__header__subtitle}>
+                <h1 className={styles.head__img__subtitle}>
                   SEU PLANNER VINTAGE
                 </h1>
               </Typography>
             </Grid>
           </Grid>
+        </div>
+
+        <div className={styles.login}>
+          <form className={styles.login__form}>
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              spacing={3}
+            >
+              <Grid item xs={8} sm={4} className={styles.login__form__title}>
+                <Typography>
+                  <h1>Login</h1>
+                </Typography>
+              </Grid>
+              <Grid item xs={8} sm={4} className={styles.login__form__textField}>
+                <TextField
+                  className={styles.login__textField__conten}
+                  error={false}
+                  label="Email"
+                  variant="filled"
+                  InputProps={{
+                    disableUnderline: true,
+                    // endAdornment: <EmailIcon style={ color: '#673ab7', margin: 12 } />,
+                  }}
+                  fullWidth
+                  // value={email}
+                  type="text"
+                  // onChange={(e) => setEmail(e.target.value)}
+                />
+              </Grid>
+
+              <Grid item xs={8} sm={4} className={styles.login__textField}>
+                <TextField
+                  className={styles.signUp__textField__content}
+                  // type={values.showPassword ? 'text' : 'password'}
+                  label="Senha"
+                  variant="filled"
+                  fullWidth
+                  
+                  name="passwordHash"
+                  // value={values.passwordHash}
+                  // onChange={(e) => setPasswordHash(e.target.value) && handleChange}
+                  InputProps={{
+                    disableUnderline: true,
+                  }}
+                />
+              </Grid>
+
+              <Grid item xs={8} sm={4}>
+                <Button variant="contained" color="primary" type="submit">
+                  Entrar
+                </Button>
+              </Grid>
+
+              <Grid item xs={8} sm={4}>
+                <Typography className={styles.login__link}>
+                  <a href="/signup">Registrar-se</a>
+                </Typography>
+              </Grid>
+            </Grid>
+          </form>
         </div>
       </main>
     </div>
