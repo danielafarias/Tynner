6dfc02ff051662d33d9cf2fa6fa1f151a0a2e5b4
Daniela Farias <farias.danicris@gmail.com>
2021-10-15 01:22:35 -0300

    Tentativa do submit task

--- client/components/InsertTask.tsx
+++ client/components/InsertTask.tsx
@@ -10,6 +10,7 @@ import styles from "../styles/InsertTask.module.scss";
 import Autocomplete from "@mui/material/Autocomplete";
 import { styled } from "@mui/material/styles";
 import { Grid, TextField, Paper, MenuItem } from "@mui/material";
+import { postTask } from "../api/Api";
 
 const tags = [
   { value: "Casa", label: "Casa" },
@@ -25,8 +26,13 @@ const priorities = [
 ];
 
 export default function FormDialog() {
-
   const [open, setOpen] = React.useState(false);
+  const [task, setTask] = React.useState("");
+  const [tag, setTag] = React.useState("Casa");
+  const [priority, setPriority] = React.useState("Alta");
+  const [date, setDate] = React.useState("");
+
+  const [error, setError] = React.useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -36,17 +42,29 @@ export default function FormDialog() {
     setOpen(false);
   };
 
-  const [tag, setTag] = React.useState("Casa");
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setTag(event.target.value);
+  // };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTag(event.target.value);
+  // const handleChangeP = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setPriority(event.target.value);
+  // };
+
+  const submitHandler = async (event: any) => {
+    event.preventDefault();
+
+    try {
+      await postTask(task, priority, date);
+      setOpen(false);
+      window.location.reload();
+      console.log("foi");
+    } catch (err) {
+      console.error(err);
+      setError(true);
+    }
   };
 
-  const [priority, setPriority] = React.useState("Alta");
-
-  const handleChangeP = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPriority(event.target.value);
-  };
+  console.log(task, priority, date);
 
   return (
     <div>
@@ -55,98 +73,97 @@ export default function FormDialog() {
       </Button>
       <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
         <DialogTitle>Nova Tarefa</DialogTitle>
-        <DialogContent >
+        <DialogContent>
           <div className={styles.content}>
-            <TextField
-              required
-              margin="dense"
-              //className={styles.field}
-              label="Tarefa"
-              InputProps={{
-                disableUnderline: true,
-              }}
-              variant="filled"
-              type="text"
-              name="task"
-              //value={task}
-              //onChange={(e) => setTask(e.target.value)}
-              fullWidth
-            />
+            <form onSubmit={submitHandler}>
+              <TextField
+                required
+                margin="dense"
+                //className={styles.field}
+                label="Tarefa"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                variant="filled"
+                type="text"
+                name="task"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                fullWidth
+              />
 
-            <TextField
-              //className={styles.field}
-              margin="dense"
-              required
-              label="Data"
-              InputProps={{
-                disableUnderline: true,
-              }}
-              InputLabelProps={{
-                shrink: true
-              }}
-              variant="filled"
-              type="date"
-              name="date"
-              //value={date}
-              //onChange={(e) => setDate(e.target.value)}
-              defaultValue="2021-01-01"
-              fullWidth
-            />
+              <TextField
+                //className={styles.field}
+                margin="dense"
+                required
+                label="Data"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="filled"
+                type="date"
+                name="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                defaultValue="2021-01-01"
+                fullWidth
+              />
 
-            <TextField
-              select
-              margin="dense"
-              //className={styles.field}
-              label="Tag"
-              InputProps={{
-                disableUnderline: true,
-              }}
-              value={tag}
-              onChange={handleChange}
-              variant="filled"
-              fullWidth
-            >
-              {tags.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
+              <TextField
+                select
+                margin="dense"
+                //className={styles.field}
+                label="Tag"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                value={tag}
+                onChange={(e) => setTag(e.target.value)}
+                variant="filled"
+                fullWidth
+              >
+                {tags.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
 
-            <TextField
-            margin="dense"
-              id="filled-select-currency"
-              select
-              //className={styles.field}
-              label="Priority"
-              InputProps={{
-                disableUnderline: true,
-              }}
-              value={priority}
-              onChange={handleChangeP}
-              variant="filled"
-              fullWidth
-            >
-              {priorities.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
+              <TextField
+                margin="dense"
+                id="filled-select-currency"
+                select
+                //className={styles.field}
+                label="Priority"
+                InputProps={{
+                  disableUnderline: true,
+                }}
+                value={priority}
+                onChange={(e) => setPriority(e.target.value)}
+                variant="filled"
+                fullWidth
+              >
+                {priorities.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <Button
+                className={styles.login__button}
+                variant="contained"
+                color="primary"
+                type="submit"
+                sx={{ backgroundColor: "#7B9E87" }}
+              >
+                Confirmar
+              </Button>
+            </form>
           </div>
         </DialogContent>
-        <DialogActions >
-          <Button
-            onClick={handleClose}
-            className={styles.login__button}
-            variant="contained"
-            color="primary"
-            type="submit"
-            sx={{ backgroundColor: "#7B9E87" }}
-          >
-            Confirmar
-          </Button>
-        </DialogActions>
       </Dialog>
     </div>
   );
