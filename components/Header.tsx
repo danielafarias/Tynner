0c8c55a646ec9a0df50d41fbc826e2dc993c7617
Daniela Farias <farias.danicris@gmail.com>
2021-10-19 23:52:08 -0300

    Organizando: Exclusão do Server; E alteração do Icon;

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+import { AppBar, Toolbar, Grid } from "@mui/material";
+import styles from "../styles/Header.module.scss";
+import Head from "next/head";
+
+export default function Header(props: any) {
+  return (
+    <header className={styles.grow}>
+       <Head>
+        <title>Tynner</title>
+        <meta name="Tynner" content="Tynner - Seu planner vintage" />
+        <link rel="icon" href="images/icon.svg"></link>
+      </Head>
+      <AppBar className={styles.bar} position="fixed">
+        <Toolbar>
+          <Grid container justifyContent="space-between" alignItems="center">
+            <Grid item>
+              <img
+                src="images/tynner.svg"
+                className={styles.logoIcon}
+                alt="Logo do site"
+              />
+            </Grid>
+            <Grid item>
+              <div className={styles.searchBar}>{props.searchBar}</div>
+            </Grid>
+            <Grid item>
+              <div className={styles.buttons}>
+                {props.button1}
+                {props.button2}
+              </div>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
+}
